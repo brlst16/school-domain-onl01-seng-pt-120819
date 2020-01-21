6e3b907525f892ac73ddb3d5c36e7bbd93e1186b
Brigette LeVert <bnl.levert@gmail.com>
2020-01-21 18:03:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 # code here!
 class School    #open class
 
-attr_accessor :name 
+attr_accessor :name   #make a reader and writer for name variable 
   
   ROSTER = {}  #create class constant in all caps 
   
   
   def initialize(name)  
-  
+  @name = name 
   end
   
   
